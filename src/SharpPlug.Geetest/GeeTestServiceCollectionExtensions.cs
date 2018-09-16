cce1ef125b57a802e5a39c84ea5e268f0fc00dae
LiangShiWei <deawback@hotmail.com>
2018-09-16 12:02:49 +0800

    add geetest

---
+++ src/SharpPlug.Geetest/GeeTestServiceCollectionExtensions.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace SharpPlug.Geetest
+{
+    public static class GeeTestServiceCollectionExtensions
+    {
+        public static IServiceCollection AddGeetest(this IServiceCollection services, Action<GeetestOptions> setupAction = null)
+        {
+            services.AddHttpClient();
+            services.Configure(setupAction);
+            services.AddSingleton<Geetest>();
+           
+            return services;
+        }
+    }
+}
